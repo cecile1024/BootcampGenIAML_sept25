e6a7fe1bb39bd55f2726ce3cd124d3208a4453fc
cecile1024 <cecile.eymond@gmail.com>
2025-10-01 21:53:38 +0200

    exo bolean mercredi soir

--- ex1_bolean_logic.py
+++ ex1_bolean_logic.py
@@ -1,7 +1,20 @@
 first="Hello World"
 #this is a comment
 print("I AM A COMPUTER")
-if 1<2 and 4>2:
+if 1<2 and 4>2: 
     print("math is fun")
 nope = None
 print(True and False)
+L=len("What is my length?")
+texte="I am shouting"
+print(texte.upper())
+nombre=int("1000")
+print(nombre)
+concatenate="4"+"real"
+print(concatenate)
+repeat=3*"cool"
+print(repeat)
+error_test=1/0
+print(error_test)
+test_type=type([])
+print(test_type)
